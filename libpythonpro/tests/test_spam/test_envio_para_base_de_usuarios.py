7b6634ce4350d66aafe6515590002ad255cd649c
jeugeniojunior <jeugenio.junior@gmail.com>
2022-06-11 17:18:34 -0300

    Implementado teste Github_api

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -23,39 +25,27 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_span(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_email(
         'jeugenio.junior@gmail.com',
         'Curso de culinaria Merendinha-JF',
         'Primeira turma do curso de culinária do Merendinha-JF'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
-
-
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_span(sessao):
     usuario = Usuario(nome='Renzo', email='renzo@pythonpro.com.br')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_email(
         'jeugenio.junior@gmail.com',
         'Curso de culinaria Merendinha-JF',
         'Primeira turma do curso de culinária do Merendinha-JF'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'jeugenio.junior@gmail.com',
         'renzo@pythonpro.com.br',
         'Curso de culinaria Merendinha-JF',
